d4a9cbebe296b1e9cf989749a9dd74f7ccf93f54
jesusgarciia23 <jesus.garciia23@gmail.com>
2015-11-28 23:22:39 -0430

    Identacion

--- html/cambio_pass_f.php
+++ html/cambio_pass_f.php
@@ -28,92 +28,50 @@
 								<h1><span class="glyphicon glyphicon-lock"></span> Cambio de Contraseña</h1>
 							</div>
 							<div class="well bs-component">
-
-
-
-
-
-
-
-							<form class="form-horizontal" method='post' action='../php/cambio_pass.php'>
-								<fieldset>
-									<legend><span class="glyphicon glyphicon-pencil"> Registro</legend>
-									<div class="form-group">
-										<label for="pass" class="col-lg-2 control-label">Nueva</label>
-										<div class="col-lg-10">
-											<input name="pass" class="form-control" id="pass" placeholder="Contraseña" type="password" title="Debe ingresar la nueva contraseña" required>
+								<form class="form-horizontal" method='post' action='../php/cambio_pass.php'>
+									<fieldset>
+										<legend><span class="glyphicon glyphicon-pencil"> Registro</legend>
+										<div class="form-group">
+											<label for="pass" class="col-lg-2 control-label">Nueva</label>
+											<div class="col-lg-10">
+												<input name="pass" class="form-control" id="pass" placeholder="Contraseña" type="password" title="Debe ingresar la nueva contraseña" required>
+											</div>
+										</div>
+										<div class="form-group">
+											<label for="re_pass" class="col-lg-2 control-label">Repita</label>
+											<div class="col-lg-10">
+												<input name="re_pass" class="form-control" id="re_pass" placeholder="Contraseña" type="password" title="Repita la contraseña que ingreso" required>
+											</div>
+										</div>
+										<div class="form-group">
+											<div class="col-lg-10 col-lg-offset-2">
+												<button type="reset" class="btn btn-default">Cancelar</button>
+												<button type="submit" class="btn btn-primary" id="cambiar">Enviar</button>
+											</div>
+										</div>
+									</fieldset>
+								</form>
+								<!-- Inicio del modal con alerta por contraseñas diferentes -->
+								<div id="myModal" class="modal fade" role="dialog">
+									<div class="modal-dialog">
+										<div class="modal-content">
+											<div class="modal-header">
+												<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove-circle"></span></button>
+												<h4 class="modal-title">Cambio de contraseña</h4>
+											</div>
+											<div class="modal-body">
+												<div class="alert alert-dismissible alert-warning">
+													<h4>¡Advertencia!</h4>
+													<p>Las Contraseñas que ha ingresado no coinciden. Por favor verifiquelas.</p>
+												</div>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
+											</div>
 										</div>
 									</div>
-									<div class="form-group">
-										<label for="re_pass" class="col-lg-2 control-label">Repita</label>
-										<div class="col-lg-10">
-											<input name="re_pass" class="form-control" id="re_pass" placeholder="Contraseña" type="password" title="Repita la contraseña que ingreso" required>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-lg-10 col-lg-offset-2">
-											<button type="reset" class="btn btn-default">Cancelar</button>
-											<button type="submit" class="btn btn-primary" id="cambiar">Enviar</button>
-										</div>
-									</div>
-								</fieldset>
-							</form>
-
-
-
-
-
-
-
-				<!-- Inicio del modal con alerta por contraseñas diferentes -->
-				<div id="myModal" class="modal fade" role="dialog">
-					<div class="modal-dialog">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove-circle"></span></button>
-								<h4 class="modal-title">Cambio de contraseña</h4>
-							</div>
-							<div class="modal-body">
-								<div class="alert alert-dismissible alert-warning">
-									<h4>¡Advertencia!</h4>
-									<p>Las Contraseñas que ha ingresado no coinciden. Por favor verifiquelas.</p>
 								</div>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- Fin del modal con alerta por contraseñas diferentes -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+								<!-- Fin del modal con alerta por contraseñas diferentes -->
 							</div>
 						</div>
 					</div>
