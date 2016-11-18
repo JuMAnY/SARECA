fa33271214d79c280e2814f68bccfc0bb332f19c
Jesús García <jesus.garciia23@gmail.com>
2016-11-18 01:12:59 -0400

    Corrección de error: campos invertidos en registro de equipos audiovisuales

--- php/equipo_audiovisual.php
+++ php/equipo_audiovisual.php
@@ -11,9 +11,9 @@
 	
 	if (isset($_POST['inf_adic'])) {
 		$inf_adic = $_POST['inf_adic'];
-		$sql = "INSERT INTO equipo_audiovisual (Serial, tipo, numero, rbn, modelo, marca, inf_adic, Estado) VALUES ('$serial','$tipo','$numero','$rbn','$marca','$modelo','$inf_adic',1)";
+		$sql = "INSERT INTO equipo_audiovisual (Serial, tipo, numero, rbn, modelo, marca, inf_adic, Estado) VALUES ('$serial','$tipo','$numero','$rbn','$modelo','$marca','$inf_adic',1)";
 	} else {
-		$sql = "INSERT INTO equipo_audiovisual (Serial, tipo, numero, rbn, modelo, marca, Estado) VALUES ('$serial','$tipo','$numero','$rbn','$marca','$modelo',1)";
+		$sql = "INSERT INTO equipo_audiovisual (Serial, tipo, numero, rbn, modelo, marca, Estado) VALUES ('$serial','$tipo','$numero','$rbn','$modelo','$marca',1)";
 	}
 
 	$res = $conectar->query($sql);
